a68a20b9d05ad55d36e747fe099597140c9099c2
Isiah Jones <isiah.jones33@outlook.com>
2021-08-25 21:27:16 -0400

    Made App the startup page. Removed old nav links.

--- MortageCalcMVC/Startup.cs
+++ MortageCalcMVC/Startup.cs
@@ -50,7 +50,7 @@ namespace MortageCalcMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=App}/{id?}");
             });
         }
     }
